9f6176db2342dc221fe527e41f107446d08cffba
Massi1399 <massi9.1.9.3@gmail.com>
2024-06-13 17:17:23 +0200

    prova 2 icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from './App.module.scss';
 import Classnames from 'classnames';
 import Mattia1Svg0 from './assets/mattia_1.svg';
 import Mattia2Svg0 from './assets/mattia_2.svg';
+import BasePng from './assets/base.png';
 
 function App() {
     const icon1Ref = useRef(null);
@@ -40,6 +41,7 @@ function App() {
 
     return (
         <div className={styles.App}>
+            <img src={BasePng} alt="" className={styles.img} />
             <img ref={icon1Ref} src={Mattia2Svg0} alt="" className={styles.icon_2_right} />
             <img ref={icon2Ref} src={Mattia1Svg0} alt="" className={styles.icon_1_right} />
         </div>
--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -5,11 +5,11 @@ export default createBoard({
     name: 'App',
     Board: () => <App />,
     environmentProps: {
-        windowWidth: 1920,
-        windowHeight: 1080,
+        windowWidth: window.innerWidth,
+        windowHeight: 6480,
         canvasBackgroundColor: '#ffffff',
         windowBackgroundColor: '#f5f5f5',
         canvasWidth: window.innerWidth,
-        canvasHeight: window.innerHeight,
-   },
+        canvasHeight: 6480,
+    },
 });
