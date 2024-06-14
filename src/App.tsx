288e0ed417bb6eaa0ff935c7e97717f5dcb1b214
tiagh3tto <tiagh3tto@gmail.com>
2024-06-14 15:37:33 +0200

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
+import React, { useEffect, useRef } from 'react';
 import styles from './App.module.scss';
-import ViteSvg from './assets/vite.svg';
-import TypescriptSvg from './assets/typescript.svg';
-import Mattia1Svg from './assets/mattia1.svg';
+import Classnames from 'classnames';
+import Mattia1Svg0 from './assets/mattia_1.svg';
+import Mattia2Svg0 from './assets/mattia_2.svg';
+import BasePng from './assets/base.png';
 
 function App() {
+    const icon1Ref = useRef(null);
+    const icon2Ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add(styles['slide-in-right']);
+                } else {
+                    entry.target.classList.remove(styles['slide-in-right']);
+                }
+            });
+        });
+
+        if (icon1Ref.current) {
+            observer.observe(icon1Ref.current);
+        }
+
+        if (icon2Ref.current) {
+            observer.observe(icon2Ref.current);
+        }
+
+        return () => {
+            if (icon1Ref.current) {
+                observer.unobserve(icon1Ref.current);
+            }
+
+            if (icon2Ref.current) {
+                observer.unobserve(icon2Ref.current);
+            }
+        };
+    }, []);
+
     return (
         <div className={styles.App}>
-            <img src={Mattia1Svg} alt="" className={styles.img1} />
+            <img src={BasePng} alt="" className={styles.img} />
+            <img ref={icon1Ref} src={Mattia2Svg0} alt="" className={styles.icon_2_right} />
+            <img ref={icon2Ref} src={Mattia1Svg0} alt="" className={styles.icon_1_right} />
         </div>
     );
 }
