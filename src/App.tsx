9eb994847c2b3607be2ddf3c0e5272047f5e10b9
“Massi1399” <massi9.1.9.3@gmail.com>
2024-06-26 19:08:18 +0200

    metà parte di mattia

--- src/App.tsx
+++ src/App.tsx
@@ -73,61 +73,58 @@ import nuvolaAhmed from './assets/nuvolaAhmed.svg';
 
 function App() {
     //Per ogni icona e testo creo un riferimento
-    const icon1Ref = useRef<HTMLImageElement>(null);
-    const icon2Ref = useRef<HTMLImageElement>(null);
-    const icon3Ref = useRef<HTMLImageElement>(null);
-    const icon4Ref = useRef<HTMLImageElement>(null);
-    const icon5Ref = useRef<HTMLImageElement>(null);
-    const icon6Ref = useRef<HTMLImageElement>(null);
-    const icon7Ref = useRef<HTMLImageElement>(null);
-    const text1Ref = useRef<HTMLDivElement>(null);
-    const text2Ref = useRef<HTMLDivElement>(null);
-    const text3Ref = useRef<HTMLDivElement>(null);
-    const text4Ref = useRef<HTMLDivElement>(null);
-    const text5Ref = useRef<HTMLDivElement>(null);
-    const text6Ref = useRef<HTMLDivElement>(null);
-    const text7Ref = useRef<HTMLDivElement>(null);
-    const text1RefPopup = useRef<HTMLDivElement>(null);
-    const text2RefPopup = useRef<HTMLDivElement>(null);
-    const text3RefPopup = useRef<HTMLDivElement>(null);
-    const text4RefPopup = useRef<HTMLDivElement>(null);
-    const text5RefPopup = useRef<HTMLDivElement>(null);
-    const text6RefPopup = useRef<HTMLDivElement>(null);
-    const text7RefPopup = useRef<HTMLDivElement>(null);
+    const mattia1Ref = useRef<HTMLImageElement>(null);
+    const mattia2Ref = useRef<HTMLImageElement>(null);
+    const mattia3Ref = useRef<HTMLImageElement>(null);
+    const mattia4Ref = useRef<HTMLImageElement>(null);
+    const mattia5Ref = useRef<HTMLImageElement>(null);
+    const mattia6Ref = useRef<HTMLImageElement>(null);
+    const mattia7Ref = useRef<HTMLImageElement>(null);
+    const mattia8Ref = useRef<HTMLImageElement>(null);
+    const mattia9Ref = useRef<HTMLImageElement>(null);
+    const mattia10Ref = useRef<HTMLImageElement>(null);
+    const mattia11Ref = useRef<HTMLImageElement>(null);
+    const mattia12Ref = useRef<HTMLImageElement>(null);
+    const mattia13Ref = useRef<HTMLImageElement>(null);
+    const mattia14Ref = useRef<HTMLImageElement>(null);
+    const mattia15Ref = useRef<HTMLImageElement>(null);
+    const mattia16Ref = useRef<HTMLImageElement>(null);
+    const mattiatext1Ref = useRef<HTMLImageElement>(null);
+    const mattiatext2Ref = useRef<HTMLImageElement>(null);
+    const mattiatext3Ref = useRef<HTMLImageElement>(null);
+    const mattiatext4Ref = useRef<HTMLImageElement>(null);
+    const mattiatext5Ref = useRef<HTMLImageElement>(null);
+    const mattiatext6Ref = useRef<HTMLImageElement>(null);
+    const mattiatext7Ref = useRef<HTMLImageElement>(null);
+    const mattiatext8Ref = useRef<HTMLImageElement>(null);
+    const mattiatext9Ref = useRef<HTMLImageElement>(null);
+    const mattiatext10Ref = useRef<HTMLImageElement>(null);
+    const mattiatext11Ref = useRef<HTMLImageElement>(null);
+    const mattiatext12Ref = useRef<HTMLImageElement>(null);
+    const mattiatext13Ref = useRef<HTMLImageElement>(null);
+    const mattiatext14Ref = useRef<HTMLImageElement>(null);
+    const mattiatext15Ref = useRef<HTMLImageElement>(null);
+    const mattiatext16Ref = useRef<HTMLImageElement>(null);
+  
 
     // Add scroll event listener
     useEffect(() => {
         const handleScroll = () => {
-            const icons = [icon1Ref, icon2Ref, icon3Ref, icon4Ref, icon5Ref, icon6Ref, icon7Ref];
-            const text = [
-                text1Ref,
-                text2Ref,
-                text3Ref,
-                text4Ref,
-                text5Ref,
-                text6Ref,
-                text7Ref,
-                text1RefPopup,
-                text2RefPopup,
-                text3RefPopup,
-                text4RefPopup,
-                text5RefPopup,
-                text6RefPopup,
-                text7RefPopup,
-            ];
+            const iconsleft = [mattia1Ref, mattia2Ref, mattia3Ref, mattia4Ref, mattia5Ref, mattia6Ref, mattia7Ref, mattia8Ref, mattia9Ref, mattia10Ref, mattia11Ref, mattia12Ref, mattia13Ref, mattia14Ref, mattia15Ref, mattia16Ref];
+            const text = [ mattiatext1Ref, mattiatext2Ref, mattiatext3Ref, mattiatext4Ref, mattiatext5Ref, mattiatext6Ref, mattiatext7Ref, mattiatext8Ref, mattiatext9Ref, mattiatext10Ref, mattiatext11Ref, mattiatext12Ref, mattiatext13Ref, mattiatext14Ref, mattiatext15Ref, mattiatext16Ref];
 
             // Controllo se l'elemento è visibile
-            icons.forEach((iconRef) => {
+            iconsleft.forEach((iconRef) => {
                 if (iconRef.current) {
                     var iconPosition = iconRef.current.getBoundingClientRect().top;
                     var windowHeight = window.innerHeight;
                     var windowCenter = windowHeight;
                     if (iconPosition < windowCenter) {
                         // Se l'icona supera il centro dello schermo aggiungo la classe per l'animazione
-                        iconRef.current.classList.add(styles['slide-in-right']);
+                        iconRef.current.classList.add(styles['slide-in-left']);
                     } else {
                         // Altrimenti rimuovo la classe
-                        iconRef.current.classList.remove(styles['slide-in-right']);
+                        iconRef.current.classList.remove(styles['slide-in-left']);
                     }
                 }
             });
@@ -156,8 +153,54 @@ function App() {
     return (
         <div className={styles.App}>
 
+                <img ref={mattia1Ref} src={Mattia1Svg0} alt="" className={styles.icon_1_left} />
+                <img ref={mattiatext1Ref} src={txtMattia1Svg} alt="" className={styles.text_1_left} />
+                
+                <img ref={mattia2Ref} src={Mattia2Svg0} alt="" className={styles.icon_2_left} />
+                <img ref={mattiatext2Ref} src={txtMattia2Svg} alt="" className={styles.text_2_left} />
 
+                <img ref={mattia3Ref} src={Mattia3Svg0} alt="" className={styles.icon_3_left} />
+                <img ref={mattiatext3Ref} src={txtMattia3Svg} alt="" className={styles.text_3_left} />
 
+                <img ref={mattia4Ref} src={Mattia4Svg0} alt="" className={styles.icon_4_left} />
+                <img ref={mattiatext4Ref} src={txtMattia4Svg} alt="" className={styles.text_4_left} />
+
+                <img ref={mattia5Ref} src={Mattia5Svg0} alt="" className={styles.icon_5_left} />
+                <img ref={mattiatext5Ref} src={txtMattia5Svg} alt="" className={styles.text_5_left} />
+
+                <img ref={mattia6Ref} src={Mattia6Svg0} alt="" className={styles.icon_6_left} />
+                <img ref={mattiatext6Ref} src={txtMattia6Svg} alt="" className={styles.text_6_left} />
+
+                <img ref={mattia7Ref} src={Mattia7Svg0} alt="" className={styles.icon_7_left} />
+                <img ref={mattiatext7Ref} src={txtMattia7Svg} alt="" className={styles.text_7_left} />
+
+                <img ref={mattia8Ref} src={Mattia8Svg0} alt="" className={styles.icon_8_left} />
+                <img ref={mattiatext8Ref} src={txtMattia8Svg} alt="" className={styles.text_8_left} />
+
+                <img ref={mattia9Ref} src={Mattia9Svg0} alt="" className={styles.icon_9_left} />
+                <img ref={mattiatext9Ref} src={txtMattia9Svg} alt="" className={styles.text_9_left} />
+
+                <img ref={mattia10Ref} src={Mattia10Svg0} alt="" className={styles.icon_10_left} />
+                <img ref={mattiatext10Ref} src={txtMattia10Svg} alt="" className={styles.text_10_left} />
+
+                <img ref={mattia11Ref} src={Mattia11Svg0} alt="" className={styles.icon_11_left} />
+                <img ref={mattiatext11Ref} src={txtMattia11Svg} alt="" className={styles.text_11_left} />
+
+                <img ref={mattia12Ref} src={Mattia12Svg0} alt="" className={styles.icon_12_left} />
+                <img ref={mattiatext12Ref} src={txtMattia12Svg} alt="" className={styles.text_12_left} />
+
+                <img ref={mattia13Ref} src={Mattia13Svg0} alt="" className={styles.icon_13_left} />
+                <img ref={mattiatext13Ref} src={txtMattia13Svg} alt="" className={styles.text_13_left} />
+
+                <img ref={mattia14Ref} src={Mattia14Svg0} alt="" className={styles.icon_14_left} />
+                <img ref={mattiatext14Ref} src={txtMattia14Svg} alt="" className={styles.text_14_left} />
+
+                <img ref={mattia15Ref} src={Mattia15Svg0} alt="" className={styles.icon_15_left} />
+                <img ref={mattiatext15Ref} src={txtMattia15Svg} alt="" className={styles.text_15_left} />
+
+                <img ref={mattia16Ref} src={Mattia16Svg0} alt="" className={styles.icon_16_left} />
+                <img ref={mattiatext16Ref} src={txtMattia16Svg} alt="" className={styles.text_16_left} />
+           
             <img src={bg1} alt="" className={styles.bg_1} />
             <img src={bg2} alt="" className={styles.bg_1} />
             <img src={bg3} alt="" className={styles.bg_1} />
@@ -165,35 +208,9 @@ function App() {
             <img src={bg5} alt="" className={styles.bg_2} />
 
             
-                <img ref={icon1Ref} src={Mattia1Svg0} alt="" className={styles.icon_1_right} />
 
-                <img ref={icon2Ref} src={Mattia2Svg0} alt="" className={styles.icon_2_right} />
-
-                <img ref={icon3Ref} src={Mattia3Svg0} alt="" className={styles.icon_3_right} />
-
-                <img ref={icon4Ref} src={Mattia4Svg0} alt="" className={styles.icon_4_right} />
-
-                <img ref={icon5Ref} src={Mattia5Svg0} alt="" className={styles.icon_5_right} />
-                
-                <img ref={icon6Ref} src={Mattia6Svg0} alt="" className={styles.icon_6_right} />
-
-                <img ref={icon7Ref} src={Mattia7Svg0} alt="" className={styles.icon_7_right} />
-            
-                <img src={nuvoleTitolo} alt="Vite Parallele" className={Classnames(styles.nuvoleTitolo, styles.AnimazioneNuvole)}/>
-                <img src={titolo} alt="Nuvole Sfondo titolo" className={Classnames(styles.titolo, styles.AnimazioneTitolo)}/>
-
-
-
-
-
-
-
-
-
-
-
-
-
+            <img src={nuvoleTitolo} alt="Vite Parallele" className={Classnames(styles.nuvoleTitolo, styles.AnimazioneNuvole)}/>
+            <img src={titolo} alt="Nuvole Sfondo titolo" className={Classnames(styles.titolo, styles.AnimazioneTitolo)}/>
 
 
         </div>
