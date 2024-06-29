6ed223d937557ae856da7c819cd40ecd56c61b4e
tiagh3tto <tiagh3tto@gmail.com>
2024-06-29 10:39:47 +0200

    new

--- src/Notification.tsx
+++ src/Notification.tsx
@@ -1,10 +1,14 @@
 // Notification.js
 import React from 'react';
+import styles from './App.module.scss';
+import Classnames from 'classnames';
 
 const Notification = () => {
     return (
-        <div style={{ textAlign: 'center', marginTop: '50%' }}>
-            <h1>Please rotate your device back to portrait mode.</h1>
+        <div className="notification-container">
+            <div className="notification-message">
+                <h1>Please rotate your device back to portrait mode.</h1>
+            </div>
         </div>
     );
 };
